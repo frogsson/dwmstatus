291d81f7c58637e5009b076369130eb121fd60ae
kim <taengoe@gmail.com>
2019-02-11 22:11:56 +0100

    more refractoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
 extern crate chrono;
 extern crate reqwest;
+extern crate serde_json;
 
-use reqwest::{get};
-use chrono::{Local};
+use chrono::Local;
 use std::string::String;
 use std::result::Result;
 use std::error::Error;
 use std::process::Command;
 
 // wttr.in
-const CITY: &str = "Orebro";
+// api.openweathermap.org/data/2.5/weather?id=217279
+// https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY
+// 2686657
+
+const CITY_ID: &str = "2686657";
 
 pub fn call(out: String) {
     println!("{}", out);
@@ -25,18 +29,39 @@ pub fn get_time() -> String {
     current_time
 }
 
-pub fn get_weather() -> Result<String, Box<dyn Error>> {
-    // wttr.in/:help
-    // wttr.in/CITY?T0
+pub fn get_weather(api_key: &str) -> Result<String, Box<dyn Error>> {
+    /* JSON_STR FORMAT
+    {
+    "base":"stations",
+    "clouds":{"all":75},
+    "cod":200,
+    "coord":{"lat":59.27,"lon":15.21},
+    "dt":1549862400,
+    "id":2686657,
+    "main":{"humidity":96,"pressure":992,"temp":274.15,"temp_max":274.15,"temp_min":274.15},
+    "name":"Orebro",
+    "sys":{"country":"SE","id":1777,"message":0.0036,"sunrise":1549867523,"sunset":1549899741,"type":1},
+    "visibility":6000,
+    "weather":[{"description":"mist","icon":"50n","id":701,"main":"Mist"}],
+    "wind":{"deg":320,"speed":1.5}
+    }
+    */
 
-    let url = format!("http://wttr.in/{}?t0", CITY);
-    let mut body = String::new();
     let mut weather = String::from("\u{e01d}");
 
-    body.push_str(&get(&url)?.text()?);
+    let url = &format!("https://api.openweathermap.org/data/2.5/weather?id={}&units=metric&appid={}", CITY_ID, api_key);
+    let json_str = &reqwest::get(url)?.text()?; // reqwest.get()?.json()? is not able to parse for some reason 
+    println!("{}", json_str);
+    let vals: serde_json::Value = serde_json::from_str(json_str)?;
+    let degrees_cel = &vals["main"]["temp"];
+    let weather_status = &vals["weather"][0]["description"].to_string();
+    let weather_status = weather_status.trim_matches('"');
+
+    weather.push_str(&format!("{} {}°C", weather_status, degrees_cel));
 
     Ok(weather)
 }
+
 
 // pub fn get_weather() -> Result<String, Box<dyn Error>> {
 //     // wttr.in/:help
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
-extern crate dwmstatus;
+extern crate rustystatus;
 
 use std::string::String;
 use std::thread::sleep;
 use std::time::{Duration, SystemTime};
+use std::fs;
 
-// https://home.openweathermap.org
+/*
+https://home.openweathermap.org
+
+API KEY
+$HOME/.config/rustystatus/apikey
+*/
 
 fn main() {
+    let api_key = fs::read_to_string("/home/kim/.config/rustystatus/apikey").unwrap();
     let mut lastweatherupdate = SystemTime::now();
-    let mut weather = match dwmstatus::get_weather() {
+    let mut weather = match rustystatus::get_weather(&api_key) {
         Ok(s) => s,
-        Err(_) => "".to_string()
+        Err(e) => { 
+            println!("{:?}", e);
+            "".to_string()
+        },
+
     };
-    let halfhour = Duration::from_secs(1800);
+    let five_min = Duration::from_secs(300);
+    let five_sec = Duration::from_secs(5);
 
     loop {
         let mut output = String::new();
 
-        if lastweatherupdate.elapsed().unwrap() >= halfhour {
-            weather = match dwmstatus::get_weather() {
+        if lastweatherupdate.elapsed().unwrap() >= five_min {
+            weather = match rustystatus::get_weather(&api_key) {
                 Ok(s) => s,
                 Err(_) => "".to_string(),
             };
@@ -26,8 +38,8 @@ fn main() {
         }
 
         output.push_str(&format!("{}", weather));
-        output.push_str(&format!(" {}", dwmstatus::get_time()));
-        dwmstatus::call(output);
-        sleep(Duration::from_secs(5));
+        output.push_str(&format!(" {}", rustystatus::get_time()));
+        rustystatus::call(output);
+        sleep(five_sec);
     }
 }
