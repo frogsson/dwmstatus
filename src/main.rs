9b8cd3b24c76508141e742095411d02667e441de
kim <taengoe@gmail.com>
2019-04-25 22:16:58 +0200

    deleting config after done with it

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 extern crate rustystatus;
 
-use rustystatus::Config;
-use rustystatus::Unwrap;
+use rustystatus::{Config, Unwrap};
 use rustystatus::{call, get_config_path, last_item, update_and_output};
 use std::error::Error;
 use std::thread::sleep;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let config = Config::from(get_config_path()?).unwrap_or_default();
+    let mut modules;
+    let update_interval;
+    let separator;
+    let len;
 
-    let separator = config.separator();
-    let mut order = config.order()?;
-    let len = order.len() - 1;
+    {
+        let config = Config::from(get_config_path()?).unwrap_or_default();
+        modules = config.modules()?;
+        update_interval = config.update_interval();
+        separator = config.separator();
+        len = modules.len() - 1;
+    }
 
     loop {
-        let output = order
+        let output: String = modules
             .iter_mut()
             .enumerate()
-            .map(|module| update_and_output(module.1, separator, last_item(module.0, len)))
-            .collect::<String>();
+            .map(|module| update_and_output(module.1, &separator, last_item(module.0, len)))
+            .collect();
 
         call(&output);
-        sleep(config.update_interval());
+        sleep(update_interval);
     }
 }
